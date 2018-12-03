8ba1ad162db8e9c1cc52163236d85adf446d89bf
Pablo Herivelton <pherivelton@gmail.com>
2018-12-03 12:27:41 -0300

    Question 987A - Codeforces

--- 987A.py
+++ 987A.py
@@ -1,9 +1,11 @@
-from decimal import Decimal
-x, y = map(int, raw_input().split())
-aux = (Decimal(x) ** (Decimal(float(y))/Decimal(float(x))))
-if aux == y:
-    print "="
-elif aux > y:
-    print ">"
-else:
-    print "<"
+dic = {"purple": "Power", "green": "Time", "blue": "Space", "orange": "Soul",
+       "red": "Reality", "yellow": "Mind"}
+colors = ["red", "purple", "green", "blue", "orange", "yellow"]
+n = int(raw_input())
+output = str(6-n)+"\n"
+for i in xrange(n):
+    color = raw_input()
+    colors.remove(color)
+for color in colors:
+    output += dic[color] + "\n"
+print output
